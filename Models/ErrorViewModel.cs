6e129412d4ce913b047b23c80f5e4721c01a6804
Bui Chi Bao <buichibao1601@gmail.com>
2024-04-16 20:13:52 +0700

    first commit

---
+++ Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace MoviesBooking.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
